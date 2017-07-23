3252c4e967d97fa6f24885dea14dbb8656d4383f
mkiterian <hmukite@gmail.com>
2017-07-24 00:44:42 +0300

    [Feature #149366719] Add bucketlist model class

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import db
 
 class User(db.Model):
     '''
-    Defines properties for a user to generate user table in db
+    Defines properties for a user to generate users table in db
     '''
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -19,4 +19,22 @@ class User(db.Model):
 
     def __str__(self):
         return "User(id='%s')" % self.id
+
+
+class Bucketlist(db.Model):
+    '''
+    Defines properties for a bucketlist, used to create bucketlists table
+    '''
+    __tablename__ = 'bucketlists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    description = db.Column(db.String(200), unique=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    items = db.relationship(
+        'Item', backref='bucketlist', lazy='dynamic')
+
+    def __init__(self, name, description, owner_id):
+        self.name = name
+        self.description = description
+        self.owner_id = owner_id
 
