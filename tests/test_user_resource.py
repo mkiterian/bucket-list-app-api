bcc3a8cb3e2529e513388c3002e6c96ac9e86a19
mkiterian <hmukite@gmail.com>
2017-07-30 07:18:26 +0300

    [Fix #149365507] Rename test test_resources file to test_user_resource

--- tests/test_user_resource.py
+++ tests/test_user_resource.py
@@ -4,37 +4,16 @@ import json
 from passlib.hash import bcrypt
 
 from app import app, db
-from app.models import User, Bucketlist, Item
+from app.models import User
+from base_testcase import BaseTest
 
 
-class UserTest(unittest.TestCase):
+class UserResourceTest(BaseTest):
     def setUp(self):
-        app.config['SECRET_KEY'] = '8h87yhggfd45dfds22as'
-        app.config['TESTING'] = True
-        app.config['WTF_CSRF_ENABLED'] = False
-        app.config['DEBUG'] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://'\
-            'localhost/db_for_api_tests'
-        self.client = app.test_client()
-        db.drop_all()
-        db.create_all()
-
-        self.headers = {"Content-Type": "application/json"}
-        self.temp_user = {
-            "username": "Sansa",
-            "email": "sansa@gmail.com",
-            "password": "wicked",
-            "confirm_password": "wicked"
-        }
-
-        self.saved_user = User("tyrion", "tyrion@gmail.com",
-                               bcrypt.hash("lion", rounds=12))
-        db.session.add(self.saved_user)
-        db.session.commit()
+        super(UserResourceTest, self).setUp()
 
     def tearDown(self):
-        db.session.remove
-        db.drop_all()
+        super(UserResourceTest, self).tearDown()
 
     def test_login_returns_ok_status_code(self):
         user = {
