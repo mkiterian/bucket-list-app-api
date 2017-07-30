de935bf2154c0b07636fb11c785b2dcca87a2ec8
mkiterian <hmukite@gmail.com>
2017-07-30 15:05:31 +0300

    [Feature #149365507] Add tests for requests with bucketlist_id

--- tests/test_bucketlist_resource.py
+++ tests/test_bucketlist_resource.py
@@ -34,6 +34,9 @@ class BucketlistResourceTest(BaseTest):
         db.session.add(self.example_bucketlist_two)
         db.session.commit()
 
+        self.bucketlist_one_id = Bucketlist.query.filter_by(
+            name="bucketlist_one").first().id
+
     def tearDown(self):
         super(BucketlistResourceTest, self).tearDown()
 
@@ -79,3 +82,59 @@ class BucketlistResourceTest(BaseTest):
             '/api/v1/bucketlists', data=json.dumps(self.user),
             headers=self.headers)
         self.assertTrue(b'Authorization Required' in response.data)
+
+    def test_successful_status_code_when_bucket_id_is_specified(self):
+        response = self.client.post(
+            '/api/v1/auth/login', data=json.dumps(self.user),
+            headers=self.headers)
+        response_content = json.loads(response.data)
+        self.headers['Authorization'] = 'JWT {}'.format(
+            response_content['access_token'])
+        response = self.client.get(
+            '/api/v1/bucketlists/{}'.format(self.bucketlist_one_id),
+            data=json.dumps(self.user),
+            headers=self.headers)
+        self.assertEqual(response.status_code, 200)
+
+    def test_bucketlist_name_is_returned_given_id(self):
+        response = self.client.post(
+            '/api/v1/auth/login', data=json.dumps(self.user),
+            headers=self.headers)
+        response_content = json.loads(response.data)
+        self.headers['Authorization'] = 'JWT {}'.format(
+            response_content['access_token'])
+        response = self.client.get(
+            '/api/v1/bucketlists/{}'.format(self.bucketlist_one_id),
+            data=json.dumps(self.user),
+            headers=self.headers)
+        
+        self.assertTrue(b'bucketlist_one' in response.data)
+
+    def test_only_one_bucketlist_is_returned_given_id(self):
+        response = self.client.post(
+            '/api/v1/auth/login', data=json.dumps(self.user),
+            headers=self.headers)
+        response_content = json.loads(response.data)
+        self.headers['Authorization'] = 'JWT {}'.format(
+            response_content['access_token'])
+        response = self.client.get(
+            '/api/v1/bucketlists/{}'.format(self.bucketlist_one_id),
+            data=json.dumps(self.user),
+            headers=self.headers)
+        
+        self.assertEqual(response.data.count(b'id'), 1)
+
+    def test_response_message_for_non_existent_id(self):
+        response = self.client.post(
+            '/api/v1/auth/login', data=json.dumps(self.user),
+            headers=self.headers)
+        response_content = json.loads(response.data)
+        self.headers['Authorization'] = 'JWT {}'.format(
+            response_content['access_token'])
+        response = self.client.get(
+            '/api/v1/bucketlists/2000',
+            data=json.dumps(self.user),
+            headers=self.headers)
+        self.assertTrue(b'requested id does not exist' in response.data)
+
+        
