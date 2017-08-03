c8dc80bc00cfd8b5281a1139bd93fc4c505b742a
mkiterian <hmukite@gmail.com>
2017-08-03 16:09:01 +0300

    [Chore #149365789] Use the development config instance

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config.from_object('config')
+app.config.from_object('config.DevelopmentConfig')
 db = SQLAlchemy(app)
 
 from . import models
