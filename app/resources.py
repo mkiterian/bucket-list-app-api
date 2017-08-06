2a203300321e192089ee875a9bed7a12f9e9694a
mkiterian <hmukite@gmail.com>
2017-08-06 10:55:34 +0300

    [Feature #149365897] Implement pagination on bucketlist resource

--- app/resources.py
+++ app/resources.py
@@ -1,5 +1,6 @@
 from passlib.hash import bcrypt
 
+from flask import jsonify, json, request
 from flask_jwt import JWT, jwt_required, current_identity
 from flask_restful import (Api, Resource, abort,
                            fields, marshal, reqparse)
@@ -86,10 +87,37 @@ class BucketlistResource(Resource):
             else:
                 return {'message': 'requested id does not exist'}, 404
         else:
-            bucketlists = Bucketlist.query.order_by(
-                Bucketlist.id.asc()).filter_by(
-                owner_id=current_identity['user_id']).all()
-            return {"bucketlists": marshal(bucketlists,
+            if (not request.args.get('page') 
+            and not request.args.get('limit')):
+                bucketlists = Bucketlist.query.order_by(
+                    Bucketlist.id.asc()).filter_by(
+                    owner_id=current_identity['user_id']).all()
+                return {"bucketlists": marshal(bucketlists,
+                                               bucketlist_fields)}
+
+            else:
+                parser = reqparse.RequestParser()
+                parser.add_argument('page',
+                                    type=int,
+                                    default=1, 
+                                    location='args')
+                parser.add_argument('limit',
+                                    type=int,
+                                    default=10,
+                                    location='args')
+                args = parser.parse_args(strict=True)
+
+                bucketlists = Bucketlist.query.order_by(
+                    Bucketlist.id.asc()).paginate(
+                        args['page'], args['limit'], error_out=False)
+
+                return {
+                    "count": len(bucketlists.items),
+                    "next": "/api/v1/bucketlists?page={}&limit={}"
+                    .format(args['page'] + 1, args['limit']),
+                    "previous": "/api/v1/bucketlists?page={}&limit={}"
+                    .format(args['page'] - 1, args['limit']),
+                    "bucketlists": marshal(bucketlists.items,
                                            bucketlist_fields)}
 
     @jwt_required()
