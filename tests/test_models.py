7bb2e1faa296b1a2e5a40b1d8f2f7240fbf93ab6
mkiterian <hmukite@gmail.com>
2017-08-17 22:45:27 +0300

    [Fix #149366719] Test if user id is created

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,25 +2,30 @@ from unittest import TestCase
 from app import app, db
 from app.models import User, Bucketlist, Item
 
+
 class UserTestCase(TestCase):
-    
+
     def setUp(self):
         app.config.from_object('config.TestingConfig')
         db.drop_all()
         db.create_all()
         self.user = User('homer', 'homer@gmail.com', 'secret')
+        db.session.add(self.user)
+        db.session.commit
 
     def tearDown(self):
         db.session.remove()
 
     def test_user_successfully_added_to_db(self):
-        db.session.add(self.user)
-        db.session.commit
-        user  = User.query.filter_by(username='homer').first()
+        user = User.query.filter_by(username='homer').first()
         self.assertTrue(self.user.id)
 
     def test_user_has_bucketlist_property(self):
         self.assertTrue(hasattr(self.user, 'bucketlists'))
+
+    def test_user_id_is_set(self):
+        user = User.query.filter_by(username='homer').first()
+        self.assertEqual(str(self.user), "User(id='1')")
 
 
 class BucketlistTestCase(TestCase):
@@ -31,7 +36,8 @@ class BucketlistTestCase(TestCase):
         self.assertTrue(hasattr(self.bucketlist, 'items'))
 
     def test_bucketlist_name_is_set(self):
-        self.assertEqual(str(self.bucketlist), 'Vacationing')
+        self.assertEqual(str(self.bucketlist),
+                         "Bucketlist(name='Vacationing')")
 
 
 class ItemTestCase(TestCase):
@@ -39,7 +45,7 @@ class ItemTestCase(TestCase):
         self.item = Item('Go to Egypt', 'It\'s a pyramid scheme', 100)
 
     def test_item_has_been_created(self):
-        self.assertEqual(str(self.item), 'Go to Egypt')
+        self.assertEqual(str(self.item.title), 'Go to Egypt')
 
     def test_item_title_is_set(self):
-        self.assertEqual(str(self.item), 'Go to Egypt')
+        self.assertEqual(str(self.item), "Item(title='Go to Egypt')")
